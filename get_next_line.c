8eeb8c784929062f26d8c12b39f6bb46e8110b33
Vitor Marcondes <vitorsergio111@gmail.com>
2022-05-25 17:38:10 -0300

    done header and .c

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,24 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 11:01:02 by vsergio           #+#    #+#             */
-/*   Updated: 2022/05/24 11:01:04 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/05/25 17:37:27 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "get_next_line.h"
+#include <sys/fcntl.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
 
-
+char	*get_next_line(int fd)
+{
+	char	*buffer_read;
+	if(fd == -1)
+	{
+		printf("Failed to read the file");
+		return (NULL);
+	}
+	read(fd, buffer_read, BUFFER_SIZE);
+	buffer_read[BUFFER_SIZE] = '\0';
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 10:59:58 by vsergio           #+#    #+#             */
-/*   Updated: 2022/05/24 11:00:56 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/05/25 15:41:58 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 20
+#endif
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 char	*get_next_line(int fd);
 
