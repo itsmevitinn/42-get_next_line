86fa3daa119d85a0147fcf035b8b4167a99d6000
Vitor Sergio <vitorsergio111@gmail.com>
2022-06-05 13:03:01 -0300

    renamed .txt and put header in .c

--- main_bonus.c
+++ main_bonus.c
@@ -1,27 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/05 13:01:51 by vsergio           #+#    #+#             */
+/*   Updated: 2022/06/05 13:01:54 by vsergio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
 
 int main(void)
 {
-	int fd = open("ola.txt", O_RDONLY);
-	int fd2 = open("salve.txt", O_RDONLY);
-	int fd5 = open("kaka.txt", O_RDONLY);
-	int fd3 = open("wigol.txt", O_RDONLY);
-	int fd4 = open("rato.txt", O_RDONLY);
+	int fd1 = open("teste1.txt", O_RDONLY);
+	int fd2 = open("teste2.txt", O_RDONLY);
+	int fd3 = open("teste3.txt", O_RDONLY);
+	int fd4 = open("teste4.txt", O_RDONLY);
+	int fd5 = open("teste5.txt", O_RDONLY);
 	char	*line;
 
 	while (1)
 	{
-		line = get_next_line(fd);
+		line = get_next_line(fd1);
 		printf("%s", line);
 		line = get_next_line(fd2);
 		printf("%s", line);
-		line = get_next_line(fd5);
+		line = get_next_line(fd3);
 		printf("%s", line);
 		line = get_next_line(fd4);
 		printf("%s", line);
-		line = get_next_line(fd3);
+		line = get_next_line(fd5);
 		printf("%s", line);
 		if (!line)
 			break;
